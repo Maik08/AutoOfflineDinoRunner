2ff741273dbddc280631dc34255481218400a301
Maik <maikm7917@gmail.com>
2024-10-15 20:37:38 +0200

    Update ChromeDinoBot.py

--- ChromeDinoBot.py
+++ ChromeDinoBot.py
@@ -13,42 +13,58 @@ def screenshot_thread():
     global latest_screenshot
     while True:
         latest_screenshot = pyautogui.screenshot()
-        time.sleep(0.01)  
+        time.sleep(0.01)
 
 def get_pixel_colors():
     global latest_screenshot
     if latest_screenshot is None:
-        return (0, 0, 0), []  
+        return (0, 0, 0), [], [] 
 
-    dino_pixel = latest_screenshot.getpixel((999, 288))  
+    dino_pixel = latest_screenshot.getpixel((999, 288))
 
-   
     cactus_pixels = [
-        latest_screenshot.getpixel((1110, 286)), 
-        latest_screenshot.getpixel((1117, 280)), 
-        latest_screenshot.getpixel((1115, 280)), 
+        latest_screenshot.getpixel((1110, 286)),
+        latest_screenshot.getpixel((1117, 280)),
+        latest_screenshot.getpixel((1115, 280)),
         latest_screenshot.getpixel((1117, 285)),
-        latest_screenshot.getpixel((1112, 283)), 
-        latest_screenshot.getpixel((1114, 280)), 
-        latest_screenshot.getpixel((1115, 280)), 
+        latest_screenshot.getpixel((1112, 283)),
+        latest_screenshot.getpixel((1114, 280)),
+        latest_screenshot.getpixel((1115, 280)),
         latest_screenshot.getpixel((1113, 280))
     ]
+
+   
+    bird_positions = [
+        (1130, 262),
+        (1140, 262),
+        (1135, 261),
+        (1132, 262),
+        (1129, 261),
+        (1128, 263)
+        
+    ]
+
     
-    return dino_pixel, cactus_pixels
+    bird_pixels = [latest_screenshot.getpixel(pos) for pos in bird_positions]
+    
+    return dino_pixel, cactus_pixels, bird_pixels  
 
 
 threading.Thread(target=screenshot_thread, daemon=True).start()
 
 while True:
-    dino_pixel, cactus_pixels = get_pixel_colors()
+    dino_pixel, cactus_pixels, bird_pixels = get_pixel_colors()
     
+   
+    if any(pixel == (172, 172, 172) for pixel in bird_pixels):  
+        jumping()
+
     
     if latest_screenshot is not None and latest_screenshot.getpixel((1272, 254)) == (8, 8, 8):
         pyautogui.press('space') 
         time.sleep(1)  
 
     background = (32, 33, 36)
-    
     
     if dino_pixel[0] == 255:  
         if any(pixel == (172, 172, 172) for pixel in cactus_pixels):    
@@ -60,5 +76,5 @@ while True:
     
     if keyboard.is_pressed('s'):
         break
-    
-    time.sleep(0.0001)  
+
+    time.sleep(0.0001)
